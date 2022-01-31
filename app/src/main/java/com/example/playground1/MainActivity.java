6a9342f9d35f6c1f8d28c73bf5db951b291d1bc6
Brian Nethercott <brianjames1997@gmail.com>
2022-01-31 16:03:02 -0500

    Commit and Push 3 - TExbox/button working, new activity creates and displays

--- app/src/main/java/com/example/playground1/MainActivity.java
+++ app/src/main/java/com/example/playground1/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.playground1;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.playground1.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,4 +18,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+    //Prereqs to show up in "onCLick" dropdown:
+    //public, void, view as only param
+    public void sendMessage (View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+
+    }
+
 }
