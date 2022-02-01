78e8e2fd5e8a04e49cb25b4524f73c92a962f392
Brian Nethercott <brianjames1997@gmail.com>
2022-01-31 20:07:15 -0500

    C&P7 - Fixes

--- app/src/main/java/com/example/playground1/MainActivity.java
+++ app/src/main/java/com/example/playground1/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 
@@ -17,17 +17,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        Button btnSend = (Button) findViewById(R.id.btnSend);
 
-    //Prereqs to show up in "onCLick" dropdown:
-    //public, void, view as only param
-    public void sendMessage (View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-
+        //Correct way to do click actions
+        btnSend.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, DisplayMessageActivity.class);
+            EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+            String message = editText.getText().toString();
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        });
 
     }
 
